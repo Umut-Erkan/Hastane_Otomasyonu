2e097d9d9a52a0b64657595db6fde143bdef83a7
Umut-Erkan <umuterkan.t@gmail.com>
2026-03-04 16:00:21 +0300

    Doktor için de bir demo oluşturdum. Çalışıyor

---
+++ DTO/DoktorDTO.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hastane_Otomasyonu.DTO
+{
+    public class DoktorDTO
+    {
+        public long Tc { get; set; }
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string Password { get; set; }
+
+        public string Eposta { get; set; } //get; init; bak
+        public string Role { get; set; }
+
+
+    }
+}
