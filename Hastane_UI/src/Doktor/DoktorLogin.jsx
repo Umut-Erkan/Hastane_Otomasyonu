935505c00c1517b535c08f2d2ae4d16d29cb3312
Umut-Erkan <umuterkan.t@gmail.com>
2026-04-07 13:46:51 +0300

    Doktorlar sadece kendi uzmanlık alanlarına ait ilaçları yazabiliyorlar. Tedavi yazılınca randevu otomatik olarak siliniyor

--- Hastane_UI/src/Doktor/DoktorLogin.jsx
+++ Hastane_UI/src/Doktor/DoktorLogin.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../Hasta/HastaStyle/Hasta_add.css'; // Aynı stil dosyasını tutarlılık için tekrar kullanıyoruz
+import '../Hasta/HastaStyle/Hasta_add.css';
 
 function DoktorLogin() {
     const navigate = useNavigate();
